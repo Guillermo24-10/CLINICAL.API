bdd20762025c985001b435754d52a10aa6b974bc
Guilleloper <pgaugusto24@gmail.com>
2024-04-11 11:56:43 -0500

    Agregar archivos de proyecto.

---
+++ src/CLINICAL.Application.Interfaces/IAnalysisRepository.cs
@@ -0,0 +1,14 @@
+﻿using CLINICAL.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLINICAL.Application.Interfaces
+{
+    public interface IAnalysisRepository
+    {
+        Task<IEnumerable<Analysis>> ListAnalysis();
+    }
+}
---
+++ src/CLINICAL.Persistence/Extensions/InjectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CLINICAL.Persistence.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLINICAL.Persistence.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionPersistencia(this IServiceCollection services)
+        {
+            services.AddSingleton<ApplicationDbContext>();//solo hay una instancia de ApplicationDbContext
+
+            return services;
+        }
+    }
+}
